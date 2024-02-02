56a232daaec41bd2475ebff30591d7cee44cb58d
AdarMhadipor1 <adar.mha@gmail.com>
2024-02-02 08:35:46 +0200

    added function declarations to vmm.h, now need to implement them

--- Src/vmm.h
+++ Src/vmm.h
@@ -3,6 +3,9 @@
 #define ENTRIES_IN_PAGE_TABLE 1024
 #define ENTRIES_IN_PAGE_DIR 1024
 
+#define PAGE_OFFSET_BITS 12
+#define PAGE_TABLE_ENTRY_BITS 10
+
 enum PAGE_PTE_FLAGS {
 
     PTE_PRESENT = 1,		    //00000000000000000000000000000001
@@ -87,3 +90,80 @@ page_directory_t* current_page_dir = 0; //physical address of the current page d
 #define PDE_GET_FRAME(addr) (addr & PDE_FRAME)
 #define PDE_IS_4MB(addr) (addr & PDE_4MB)
 #define PDE_SET_FRAME(addrOfPtEntry, frame) (*(uint32_t*)addrOfPtEntry) = ((*(uint32_t*)addrOfPtEntry) & ~PDE_FRAME) | ((uint32_t)frame)
+
+//These macros are used to break down a virtual address into its physical parts. according to little os book section 9.2
+#define PD_INDEX(virtual_address) (virtual_address >> PAGE_TABLE_ENTRY_BITS >> PAGE_OFFSET_BITS) //takes a virtual address and finds the PDE of the virtual address
+#define PT_INDEX(virtual_address) (virtual_address >> PAGE_OFFSET_BITS) & 0x3FF //we need the bits from 12 to 21. so we move them to start, and then end & removes the bits used by the PDE
+#define PAGE_INDEX(virtual_address) (virtual_address & 0xFFF) //takes only the 12 bits on the right of the virtual address, which specify the index in the page.
+
+#define SET_ATTRIBUTE(addr, attr) (*(uint32_t*)addr |= attr)
+#define CLEAR_ATTRIBUTE(addr, attr) (*(uint32_t*)addr &= ~(attr))
+
+/*
+this function initializes the virtual memory manager.
+creating a page directory and assigning it to cr3 register.
+setting the Paging bit in cr0 register
+return value: 1 if successful, otherwise 0.
+*/
+uint32_t initialize_vmm();
+
+/*
+this function takes a virtual address and returns its page.
+it finds the PDE using the PD_INDEX macro, and then finds the page in that PDE using the PT_INDEX macro.
+virtual_address: the address of the page
+return value: the page as a pointer to a page_table_entry_t struct.
+*/
+page_table_entry_t* get_page(const void* virtual_address);
+
+
+/*
+this function takes a virtual address and returns the page table (PDE) the virtual address resides in.
+virtual_address: the address we are indexing.
+return value: a pointer to a page_dir_entry_t struct, which represents a page table.
+*/
+page_dir_entry_t* get_page_table(const void* virtual_address);
+
+
+/*
+this function takes a virtual address, and will assaign the physical address physical_address as the physical frame of the page.
+virtual_address: the virtual address that gets asigned a page.
+physical_address: the address of the page to assaign, assuming 0x1000 alligned.
+return value: 1 if success, else 0
+*/
+uint32_t map_page(const void* virtual_address, const void* physical_address);
+
+/*
+this function takes a page, and unmaps it from a frame. it will free the frame, set the 20 bits of the frame as clear, and will clear the PRESENT bit.
+page: the page to unmap from its frame.
+return value: None
+*/
+void unmap_page(page_table_entry_t* page);
+
+/*
+this function takes a page and allocates a frame to it.
+page: the page we allocate a frame for.
+return value: the address of the frame we allocated.
+*/
+void* allocate_block(page_table_entry_t* page);
+
+/*
+this function takes a page and frees the frame in it.
+page: the page we free its frame
+return value: None.
+*/
+void free_block(page_table_entry_t* page);
+
+/*
+this function takes a pointer to a page directory, and sets the page directory to be the current page directory.
+edit the current_page_dir pointer, and modify cr3 register.
+dir: the directory to set as current
+return value: 1 of succsessful, otherwise 0.
+*/
+uint32_t set_page_directory(page_directory_t* dir);
+
+/*
+this function takes a virtual address, and flushes it from the TLB. the TLB is a table that stores cache about virtual->physical addresses, to improve runtime. if a page was chanegd, we need to flush the TLB entry of that address.
+virtual_address: the virtual address to flush.
+return value: None.
+*/
+void flush_tlb_address(const void* virtual_address);
